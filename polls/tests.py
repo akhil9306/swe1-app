b7f8087e2dc72ecba15e925107c7941a8860829f
Akhil Vardhan <akhilvardhan27@gmail.com>
2025-10-19 02:44:22 -0400

    Feat: Perform black and flake8 commands and Add polls/test.py

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+
+class SmokeTests(TestCase):
+    def test_index_loads(self):
+        client = Client()
+        response = client.get("/polls/")
+        self.assertEqual(response.status_code, 200)
